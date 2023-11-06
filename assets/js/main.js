32cd8bbd8ba8aa07ddff8a2fbf6dcac402304fd7
ThePeaces <tegarmascart@gmail.com>
2023-11-06 14:24:16 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+//  Toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            //  active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // sticky header
+    let Header = document.querySelector('header');
+
+    Header.classList.toggle('sticky', window.scrollY > 100);
+
+    //  remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+// loader nw
+document.addEventListener('DOMContentLoaded', () => {
+    const pageLoader = document.querySelector('.page-loader');
+    if (pageLoader) {
+        // Mengatur opacity ke 0 untuk efek fading
+        pageLoader.remove()
+        
+        // // Mengatur event transitionend untuk menghapus loader setelah fading selesai
+        // pageLoader.addEventListener('transitionend', () => {
+        //     pageLoader.style.display = 'none';
+        // });
+    }
+});
+
