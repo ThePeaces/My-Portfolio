c3778b395ca1154f6282d2faac000ced43b81f57
ThePeaces <tegarmascart@gmail.com>
2023-11-11 22:24:28 +0700

    first commit

--- main/assets/js/landingpage.js
+++ main/assets/js/landingpage.js
@@ -1,13 +1,7 @@
-// loader nw
-document.addEventListener('DOMContentLoaded', () => {
-    const pageLoader = document.querySelector('.page-loader');
-    if (pageLoader) {
-        // Mengatur opacity ke 0 untuk efek fading
-        pageLoader.remove()
-        
-        // // Mengatur event transitionend untuk menghapus loader setelah fading selesai
-        // pageLoader.addEventListener('transitionend', () => {
-        //     pageLoader.style.display = 'none';
-        // });
-    }
-});
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
